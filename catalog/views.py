22cd3b6345a74826464e443d27ea7f31dc3321ac
Artemova Dina <artemova@perx.ru>
2025-08-16 17:57:51 +0300

    add controllers

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
+    """Функция рендера домашней страницы"""
+
     return render(request, 'home.html')
 
 
 def contacts(request):
+    """Функция обработки запросов страницы контактов"""
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(name)
+        print(phone)
+        print(message)
+
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'contacts.html')

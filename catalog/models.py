b11d7264f90a177a898ba28e344cb02ccd4cbc7e
Artemova Dina <artemova@perx.ru>
2025-08-18 21:07:27 +0300

    create models and manage admin panel

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель категории товара"""
+
+    name = models.CharField(max_length=50, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", null=True, blank=True,
+                                   help_text="Введите описание категории товаров")
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
+
+    def __str__(self):
+        """Строковое представление объекта категории"""
+
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    """Модель продукта"""
+
+    name = models.CharField(max_length=50, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", null=True, blank=True, help_text="Введите описание товара")
+    image = models.ImageField(upload_to="media/", null=True, blank=True, verbose_name="Изображение",
+                              help_text="Загрузите изображение товара")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="products")
+    price = models.FloatField(verbose_name="Цена", help_text="Укажите стоимость единицы товара")
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата обновления", auto_now=True)
+
+    def __str__(self):
+        """Строковое представление объекта продукта"""
+
+        return f"{self.name}. Цена: {self.price}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ["name", "price", "created_at"]

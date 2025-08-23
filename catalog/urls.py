4a950a8e32e23a07eb89788887b2aebf5e0059bf
Artemova Dina <artemova@perx.ru>
2025-08-23 11:22:17 +0300

    rewrite catalog app to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product_detail, add_product
+from catalog.views import ProductListView, ProductDetailView, ContactListView, ProductCreateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product_detail/<int:product_id>/', product_detail, name='product_detail'),
-    path('add_product/', add_product, name='add_product')
+    path('', ProductListView.as_view(), name='home'),
+    path('contacts/', ContactListView.as_view(), name='contacts'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('add_product/', ProductCreateView.as_view(), name='add_product')
 ]
